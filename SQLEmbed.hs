bfd4ffffe01ff2f950dc4e4c605183d988ef21ee
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2015-05-09 17:14:33 -0400

    Checkpoint: removing polykind from Column

--- SQLEmbed.hs
+++ SQLEmbed.hs
@@ -17,10 +17,14 @@ Portability : non-portable (GHC only)
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE ConstraintKinds #-}
 
 import GHC.TypeLits
+import GHC.Exts (Constraint)
 import Data.Proxy
 import Data.List (intersperse)
+import qualified Database.PostgreSQL.Simple as P
+import qualified Database.PostgreSQL.Simple.ToField as PTF
 
 type family NewElement (s :: k) (ss :: [k]) :: Bool where
   NewElement s '[] = 'True
@@ -38,6 +42,13 @@ type family Fst (t :: (k, l)) :: k where
 type family Fsts (ss :: [(k, l)]) :: [k] where
   Fsts '[] = '[]
   Fsts ('(x, y) ': rest) = x ': (Fsts rest)
+
+type family Snd (t :: (k, l)) :: l where
+  Snd '(x, y) = y
+
+type family Snds (ss :: [(k, l)]) :: [l] where
+  Snds '[] = '[]
+  Snds ('(x, y) ': rest) = y ': (Snds rest)
 
 type family Elem (x :: k) (xs :: [k]) :: Bool where
   Elem x '[] = 'False
@@ -67,6 +78,15 @@ type family Union (xs :: [k]) (ys :: [k]) :: [k] where
   Union xs '[] = xs
   Union (x ': xs) ys = IfElse (Elem x ys) ys (Union xs (x ': ys))
 
+type family Append (xs :: [k]) (ys :: [k]) :: [k] where
+  Append '[] ys = ys
+  Append xs '[] = xs
+  Append (x ': xs) ys = x ': (Append xs ys)
+
+type family Every (c :: k -> Constraint) (xs :: [k]) :: Constraint where
+  Every c '[] = ()
+  Every c (x ': xs) = (c x, Every c xs)
+
 {-
 data UniqueSymbols :: [Symbol] -> * where
   NilSymbols :: UniqueSymbols '[]
@@ -85,6 +105,27 @@ data Universe :: * where
   UText :: Universe
   UInt :: Universe
   UBool :: Universe
+  UNull :: Universe
+
+{-
+type family Concrete (u :: Universe) :: * where
+  Concrete UText = String
+  Concrete UInt = Int
+  Concrete UBool = Bool
+-}
+
+data Concretion :: Universe -> * where
+  CText :: String -> Concretion UText
+  CInt :: Int -> Concretion UInt
+  CBool :: Bool -> Concretion UBool
+  CNull :: () -> Concretion UNull
+
+showConcretion :: Concretion u -> String
+showConcretion c = case c of
+    CText str -> str
+    CInt i -> show i
+    CBool b -> show b
+    CNull _ -> "NULL"
 
 -- | A Column is a string identifier (Symbol) and an identifier of some type.
 data Column :: Symbol -> k -> * where
@@ -104,6 +145,12 @@ column3 = Column (Proxy :: Proxy "value") (Proxy :: Proxy UBool)
 
 column4 :: Column "id" UText
 column4 = Column (Proxy :: Proxy "id") (Proxy :: Proxy UText)
+
+column5 :: Column "test1" Char
+column5 = Column (Proxy :: Proxy "test1") (Proxy :: Proxy Char)
+
+column6 :: Column "test2" (Maybe Int)
+column6 = Column (Proxy :: Proxy "test2") (Proxy :: Proxy (Maybe Int))
 
 --data Schema :: [Symbol] -> [k] -> * where
 --  EmptySchema :: Schema '[] '[]
@@ -125,26 +172,26 @@ type instance Value UText = String
 type instance Value UInt = Int
 type instance Value UBool = Bool
 
-data Value' t where
-  MkVal :: t -> Value' t
+data Condition :: [(Symbol, k)] -> * where
+  EmptyCondition :: Condition '[]
+  EqCondition :: Column sym t -> Value t -> Condition '[ '(sym, t) ]
+  AndCondition :: Condition cs -> Condition cs' -> Condition (Append cs cs')
+  OrCondition :: Condition cs -> Condition cs' -> Condition (Append cs cs')
+  -- ^ We Append rather than Union so that the length of the type list parameter
+  --   will be exactly the number of parameter substitutions required to do
+  --   a DB query.
 
-data Constraint :: [(Symbol, k)] -> * where
-  EmptyConstraint :: Constraint '[]
-  EqConstraint :: Column sym t -> Value t -> Constraint '[ '(sym, t) ]
-  AndConstraint :: Constraint cs -> Constraint cs' -> Constraint (Union cs cs')
-  OrConstraint :: Constraint cs -> Constraint cs' -> Constraint (Union cs cs')
+constraint1 = EqCondition column1 1
 
-constraint1 = EqConstraint column1 1
+constraint2 = EqCondition column2 "Foobar"
 
-constraint2 = EqConstraint column2 "Foobar"
+constraint3 = EqCondition column3 False
 
-constraint3 = EqConstraint column3 False
+constraint4 = EqCondition column1 2
 
-constraint4 = EqConstraint column1 2
+constraint5 = AndCondition constraint1 (AndCondition constraint2 constraint4)
 
-constraint5 = AndConstraint constraint1 (AndConstraint constraint2 constraint4)
-
-constraint6 = AndConstraint constraint1 constraint2
+constraint6 = AndCondition constraint1 constraint2
 
 -- | A Select is like a schema, but there can be duplicate columns.
 data Select :: [(Symbol, k)] -> * where
@@ -155,9 +202,9 @@ select1 = ConsSelect column2 EmptySelect
 
 select2 = ConsSelect column1 select1
 
--- | A Query is a Select and a Constraint.
+-- | A Query is a Select and a Condition.
 data Query :: [(Symbol, k)] -> [(Symbol, k)] -> * where
-  Query :: Select ss -> Constraint cs -> Query ss cs
+  Query :: Select ss -> Condition cs -> Query ss cs
 
 query1 = Query select1 constraint6
 
@@ -184,8 +231,8 @@ queryOnTable1 = QueryOnTable query1 table1
 
 -- NOTE we cannot do this in general!!! The select and constraints must be
 -- against Universe!
-makeSQL :: QueryOnTable selected available -> String
-makeSQL (QueryOnTable (Query select constrain) table) = 
+makeQuery :: QueryOnTable selected available -> String
+makeQuery (QueryOnTable (Query select constrain) table) = 
     concat
     [ "SELECT "
     , selectClause
@@ -195,11 +242,11 @@ makeSQL (QueryOnTable (Query select constrain) table) =
     , conditionClause
     ]
   where
-    selectClause = makeSQLSelectClause select
-    conditionClause = makeSQLConstraintClause constrain
+    selectClause = makeQuerySelectClause select
+    conditionClause = makeQueryConditionClause constrain
 
-makeSQLSelectClause :: Select ss -> String
-makeSQLSelectClause sel = concat ["(", makeSelectFields sel, ")"]
+makeQuerySelectClause :: Select ss -> String
+makeQuerySelectClause sel = concat ["(", makeSelectFields sel, ")"]
   where
 
     makeSelectFields :: Select ss -> String
@@ -211,12 +258,59 @@ makeSQLSelectClause sel = concat ["(", makeSelectFields sel, ")"]
         ConsSelect col rest -> columnName col : makeSelectFields' rest
         
 
-makeSQLConstraintClause :: Constraint cs -> String
-makeSQLConstraintClause constr = case constr of
-  EmptyConstraint -> "1=1" -- Can we put "True" ? or "true" ?
-  EqConstraint col val -> concat [columnName col, " = ", valueToString val]
-  AndConstraint left right -> concat [makeSQLConstraintClause left, " AND ", makeSQLConstraintClause right]
-  OrConstraint left right -> concat [makeSQLConstraintClause left, " OR ", makeSQLConstraintClause right]
+makeQueryConditionClause :: Condition cs -> String
+makeQueryConditionClause constr = case constr of
+  EmptyCondition -> "1=1" -- Can we put "True" ? or "true" ?
+  -- We use a ? because query parameter substitution shall be done by another
+  -- system, namely postgresql-simple.
+  EqCondition col val -> concat [columnName col, " = ?"]
+  AndCondition left right -> concat [makeQueryConditionClause left, " AND ", makeQueryConditionClause right]
+  OrCondition left right -> concat [makeQueryConditionClause left, " OR ", makeQueryConditionClause right]
 
--- This is the focus now...
-valueToString _ = "?"
+
+-- We are able to make a SELECT clause with lots of static guarantees; cool.
+-- Next up, we need to produce parameters for the WHERE clause.
+-- Since we're targeting postgresql-simple, we shall demand that every Value t
+-- in the constraint can be made into a ToField instance.
+-- No I think it will be easier if we just demand that every Value can be
+-- injected into some fixed type like Universe, and then define ToField on
+-- Universe via ToField on underlying types like Int and Text. I say it's
+-- easier because then we can just make a [Universe] as the q parameter for
+-- the query function.
+
+data Semantics :: [k] -> [l] -> * where
+  EmptySemantics :: Semantics '[] '[]
+  ConsSemantics :: (NewElement k ks ~ 'True) => (Value k -> Value l) -> Semantics ks ls -> Semantics (k ': ks) (l ': ls)
+  -- ^ Constraint ensures each domain has at most one function associated with it.
+
+data HList :: [*] -> * where
+  EmptyHList :: HList '[]
+  ConsHList :: t -> HList ts -> HList (t ': ts)
+
+applySemantics :: Semantics domains codomains -> HList domains -> HList codomains
+applySemantics = undefined
+
+uniformity
+  :: ( Every ((~) Bool) codomains
+     )
+  => Semantics domains codomains
+  -> ()
+uniformity _ = ()
+
+ex1 = uniformity EmptySemantics
+
+--ex2 = uniformity (ConsSemantics (\x -> not x) EmptySemantics)
+
+--
+-- Also note, we'll need to deal with the row type as well. Should that be
+-- part of the QueryOnTable?
+
+--makeParameters :: QueryOnTable selected available -> 
+
+-- What if we have a list of functions, for each (_, t) in that list, which map
+-- something of type t to something of type s?
+-- Before even that, we need a way to have a type depend upon a kind; we need
+-- to get something of type "the column's second parameter".
+-- I suppose we could use the Value family for now... It just means that
+-- every domain-specific universe has to define these.
+
