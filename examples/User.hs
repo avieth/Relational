4b6133aa812fc0efbaf27003a94b4bd1f3805ef1
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2015-05-11 01:44:17 -0400

    Update user example

--- examples/User.hs
+++ examples/User.hs
@@ -15,6 +15,7 @@ Portability : non-portable (GHC only)
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PatternSynonyms #-}
 
+import Control.Applicative
 import Data.Proxy
 import Data.Relational
 import Data.Relational.PostgreSQL
@@ -29,22 +30,22 @@ newtype Age = Age Int
   deriving (Show)
 
 instance InUniverse PostgresUniverse Username where
-  type Representation PostgresUniverse Username = T.Text
-  toUniverse proxyU _ = toUniverse proxyU (Proxy :: Proxy T.Text)
-  toRepresentation proxy (Username t) = t
-  fromRepresentation proxy = Just . Username
+  type UniverseType PostgresUniverse Username = T.Text
+  toUniverse proxyU (Username t) = UText t
+  fromUniverse proxy (UText t) = Just (Username t)
+  toUniverse' proxy = UText
 
 instance InUniverse PostgresUniverse Fullname where
-  type Representation PostgresUniverse Fullname = T.Text
-  toUniverse proxyU _ = toUniverse proxyU (Proxy :: Proxy T.Text)
-  toRepresentation proxy (Fullname t) = t
-  fromRepresentation proxy = Just . Fullname
+  type UniverseType PostgresUniverse Fullname = T.Text
+  toUniverse proxyU (Fullname t) = UText t
+  fromUniverse proxy (UText t) = Just (Fullname t)
+  toUniverse' proxy = UText
 
 instance InUniverse PostgresUniverse Age where
-  type Representation PostgresUniverse Age = Int
-  toUniverse proxyU _ = toUniverse proxyU (Proxy :: Proxy Int)
-  toRepresentation proxy (Age i) = i
-  fromRepresentation proxy = Just . Age
+  type UniverseType PostgresUniverse Age = Int
+  toUniverse proxyU (Age i) = UInt i
+  fromUniverse proxy (UInt i) = Just (Age i)
+  toUniverse' proxy = UInt
 
 data UserProfile = UserProfile Username Fullname Age
   deriving (Show)
@@ -85,9 +86,23 @@ fetchProfileForUsername uname =
         Query
           (userNameColumn .+| fullNameColumn .+| ageColumn .+| nil)
           (userNameColumn .==. uname)
-    convertToUserProfile :: HList '[T.Text, T.Text, Int] -> UserProfile
+    convertToUserProfile :: HList '[PostgresUniverse Username, PostgresUniverse Fullname, PostgresUniverse Age] -> Maybe UserProfile
     convertToUserProfile hlist = case hlist of
-        username :> fullname :> age :> HNil -> UserProfile (Username username) (Fullname fullname) (Age age)
+        username :> fullname :> age :> HNil ->
+            let proxyUsername :: Proxy Username
+                proxyUsername = Proxy
+                proxyFullname :: Proxy Fullname
+                proxyFullname = Proxy
+                proxyAge :: Proxy age
+                proxyAge = Proxy
+            in  UserProfile <$> fromUniverse proxyUsername username <*> fromUniverse proxyFullname fullname <*> fromUniverse proxyAge age
+
+exampleSelect username = do
+    conn <- P.connect (P.defaultConnectInfo { P.connectUser = "alex" })
+    rows <- postgresSelect (fetchProfileForUsername username) conn
+    print rows
+    return ()
+
 {-
 selectUserProfile up =
     Select
@@ -105,6 +120,7 @@ selectUserProfile up =
           (completeCharacterization schema )
 -}
 
+insertUserProfile :: UserProfile -> Insert PostgresUniverse "user_profiles" UserProfileSchema
 insertUserProfile (UserProfile username fullname age) =
     Insert
       proxy
@@ -113,49 +129,11 @@ insertUserProfile (UserProfile username fullname age) =
   where
     proxy :: Proxy PostgresUniverse
     proxy = Proxy
-    row :: HList '[ T.Text, T.Text, Int ]
-    row =    (toRepresentation proxy username)
-          :> (toRepresentation proxy fullname)
-          :> (toRepresentation proxy age)
+    row :: HList '[ Username, Fullname, Age ]
+    row =    (username)
+          :> (fullname)
+          :> (age)
           :> HNil
-
-deleteUserProfile uname =
-    Delete proxy userProfileTable (userNameColumn .==. uname)
-  where
-    proxy :: Proxy PostgresUniverse
-    proxy = Proxy
-
-updateUserProfileUsername oldUsername newUsername =
-    Update proxy userProfileTable projection (userNameColumn .==. oldUsername) ((toRepresentation proxy newUsername) :> HNil)
-  where
-    proxy :: Proxy PostgresUniverse
-    proxy = Proxy
-    projection = userNameColumn .+| nil
-
-{-
-updateUserProfileFullnameAge
-  :: Username
-  -> Fullname
-  -> Age
-  -> Update PostgresUniverse "user_profiles" UserProfileSchema '[ '("fullname", Fullname), '("age", Age) ] '[ '("username", Username) ]
-updateUserProfileFullnameAge username newFullname newAge =
-    Update proxy userProfileTable projection (userNameColumn .==. username) columns
-  where
-    proxy :: Proxy PostgresUniverse
-    proxy = Proxy
-    projection :: Project '[ '("fullname", Fullname), '("age", Age) ]
-    projection = fullNameColumn .+| ageColumn .+| nil
-    columns :: HList '[ T.Text, Int ]
-    columns =    (toRepresentation proxy newFullname)
-              :> (toRepresentation proxy newAge)
-              :> HNil
--}
-
-exampleSelect username = do
-    conn <- P.connect (P.defaultConnectInfo { P.connectUser = "alex" })
-    rows <- postgresSelect (fetchProfileForUsername username) conn
-    print rows
-    return ()
 
 exampleInsert userProfile = do
     conn <- P.connect (P.defaultConnectInfo { P.connectUser = "alex" })
@@ -163,22 +141,63 @@ exampleInsert userProfile = do
     print i
     return ()
 
+deleteUserProfile :: Username -> Delete PostgresUniverse "user_profiles" UserProfileSchema '[ '("username", Username) ]
+deleteUserProfile uname =
+    Delete proxy userProfileTable (userNameColumn .==. uname)
+  where
+    proxy :: Proxy PostgresUniverse
+    proxy = Proxy
+
 exampleDelete username = do
     conn <- P.connect (P.defaultConnectInfo { P.connectUser = "alex" })
     i <- postgresDelete (deleteUserProfile username) conn
     print i
     return ()
 
-{-
+updateUserProfileUsername
+  :: Username
+  -> Username
+  -> Update PostgresUniverse "user_profiles" UserProfileSchema '[ '("username", Username) ] '[ '("username", Username) ]
+updateUserProfileUsername oldUsername newUsername =
+    Update
+      proxy
+      userProfileTable
+      projection
+      (userNameColumn .==. oldUsername)
+      (newUsername :> HNil)
+  where
+    proxy :: Proxy PostgresUniverse
+    proxy = Proxy
+    projection = userNameColumn .+| nil
+
 exampleUpdate1 oldUsername newUsername = do
     conn <- P.connect (P.defaultConnectInfo { P.connectUser = "alex" })
     i <- postgresUpdate (updateUserProfileUsername oldUsername newUsername) conn
     print i
     return ()
 
+updateUserProfileFullnameAge
+  :: Username
+  -> Fullname
+  -> Age
+  -> Update PostgresUniverse "user_profiles" UserProfileSchema '[ '("fullname", Fullname), '("age", Age) ] '[ '("username", Username) ]
+updateUserProfileFullnameAge username newFullname newAge =
+    Update
+      proxy
+      userProfileTable
+      projection
+      (userNameColumn .==. username)
+      columns
+  where
+    proxy :: Proxy PostgresUniverse
+    proxy = Proxy
+    projection :: Project '[ '("fullname", Fullname), '("age", Age) ]
+    projection = fullNameColumn .+| ageColumn .+| nil
+    columns :: HList '[ Fullname, Age ]
+    columns = newFullname :> newAge :> HNil
+
 exampleUpdate2 username newFullname newAge = do
     conn <- P.connect (P.defaultConnectInfo { P.connectUser = "alex" })
     i <- postgresUpdate (updateUserProfileFullnameAge username newFullname newAge) conn
     print i
     return ()
--}
