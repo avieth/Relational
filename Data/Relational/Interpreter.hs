62030b37756278f2bc59521d2d8a4ac0ad05a28a
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2015-05-13 19:52:11 -0400

    interpreter convert selected HList back to Rows

--- Data/Relational/Interpreter.hs
+++ Data/Relational/Interpreter.hs
@@ -12,15 +12,25 @@ Portability : non-portable (GHC only)
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Data.Relational.Interpreter (
 
     RelationalInterpreter(..)
+  , interpretSelect'
+  , ConvertToRow
+  , convertToRow
 
   ) where
 
 import GHC.Exts (Constraint)
 import GHC.TypeLits (Symbol)
+import Control.Applicative
 import Data.Proxy
 import Data.Relational
 
@@ -61,3 +71,43 @@ class RelationalInterpreter t where
     => Proxy t
     -> Update '(tableName, schema) projected conditioned
     -> (InterpreterMonad t) ()
+
+class ConvertToRow universe (projected :: [(Symbol, *)]) where
+  convertToRow
+    :: (Every (InUniverse universe) (Snds projected))
+    => Proxy universe
+    -> Project projected
+    -> HList (Fmap universe (Snds projected))
+    -> Maybe (Row projected)
+
+instance ConvertToRow universe '[] where
+  convertToRow proxy EndProject HNil = Just EndRow
+
+instance ConvertToRow universe ts => ConvertToRow universe ( '(sym, t) ': ts) where
+  convertToRow proxy (x :+| prest) (v :> hrest) = case fromUniverse (Proxy :: Proxy t) v of
+      Nothing -> Nothing
+      Just v' -> (:&|) <$> (pure (fromColumnAndValue x v')) <*> (convertToRow proxy prest hrest)
+
+-- | interpretSelect gives an HList of values, but loses their column names
+--   and wraps them all in the Universe t datatype. This function tries to
+--   return those HLists to Row datatypes, using the Project from the Select
+--   clause to recover the Row's form and column names, and the universe of
+--   the interpreter to convert the values.
+interpretSelect'
+  :: forall t tableName schema projected conditioned .
+     ( RelationalInterpreter t
+     , Every (InUniverse (Universe t)) (Snds projected)
+     , Every (InUniverse (Universe t)) (Snds (Concat conditioned))
+     , InterpreterSelectConstraint t schema projected conditioned
+     , Functor (InterpreterMonad t)
+     , ConvertToRow (Universe t) projected
+     )
+  => Proxy t
+  -> Select '(tableName, schema) projected conditioned
+  -> (InterpreterMonad t) [Maybe (Row projected)]
+interpretSelect' proxyT select =
+    let interpretation = interpretSelect proxyT select
+    in  (fmap . fmap) (convertToRow proxyU (selectProjection select)) interpretation
+  where
+    proxyU :: Proxy (Universe t)
+    proxyU = Proxy
